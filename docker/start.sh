3c3a49f386813a42ff8694561ac8cf14ed59569f
Dushyant Khosla <dushyant.khosla@yahoo.com>
2018-06-05 15:03:30 +0200

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -4,9 +4,6 @@ source activate ds-env
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_MAIL
 
-git config --global http.proxy http://squid.service.pmicicd.ocean:3128
-cp /root/pip.conf /etc/pip.conf
-
 alias docker='sudo docker'
 
 # Print information
@@ -15,17 +12,13 @@ echo ""
 echo ""
 echo "To start a Jupyter notebook server, run ..."
 echo "-----------------------------------------------------------------"
-echo "jupyter lab --allow-root --no-browser --ip 0.0.0.0"
+echo "jupyter lab --allow-root --no-browser --ip 0.0.0.0 &> /dev/null &"
+echo "jupyter notebook list"
 echo ""
 echo "Configure git. Run ..."
 echo "-----------------------------------------------------------------"
 echo "git config --global user.email 'you@example.com'"
 echo "git config --global user.name 'Your Name'"
-echo ""
-echo "If git or pip doesn't work, configure proxies:"
-echo "---------------------------------------------------------------------------------"
-echo "git config --global http.proxy http://squid.service.pmicicd.ocean:3128"
-echo "cp /root/pip.conf /etc/pip.conf"
 echo ""
 echo ""
 echo ""
